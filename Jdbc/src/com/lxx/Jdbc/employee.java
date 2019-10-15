c910b4f5a93151a3b3f070c5821a5b4a4ee80bf1
lxx <1710497214@qq.com>
2019-10-15 17:11:09 +0800

    新增数据-mysql性能调优

--- Jdbc/src/com/lxx/Jdbc/employee.java
+++ Jdbc/src/com/lxx/Jdbc/employee.java
@@ -2,15 +2,19 @@ package com.lxx.Jdbc;
 
 import java.io.FileReader;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.Properties;
+import java.util.Random;
+import java.util.logging.SimpleFormatter;
 public class employee {
 
 	public static void main(String[] args) {
 		Connection conn=null;
-		Statement stmt=null;
+		PreparedStatement pstmt=null;
 //		PreparedStatement ps=null;Statement锟斤拷锟斤拷锟斤拷
 		int count=0;
 		try {
@@ -31,20 +35,38 @@ public class employee {
 			conn=DriverManager.getConnection(url, user, password);
 			//锟斤拷锟斤拷锟斤拷锟斤拷
 			conn.setAutoCommit(false);
-			stmt=conn.createStatement();
-//			String sql="insert into employee(name,pwd) value('"+name+"','"+pwd+"')";
-			String sql="INSERT INTO user(name,usex,address,date,person,girl,school,studentid,height,weight)\r\n" + 
-					"VALUES('狗狗','宠物','湖南省长沙市芙蓉区东方新城H区5456街道'\r\n" + 
-					",NOW(),'李四哇','王雪阳','邵阳市第一中学',43052319981202,20.00,10.38);";
+			String[] name=new String[] {"狗狗","海豚","北极熊","猫猫","黄牛","熊猫","袋鼠","猪","猿"};
+			String[] address=new String[] {"湖南省长沙市芙蓉区","深圳市腾讯中心","北京市东城区大兴亦庄","上海市浦东新区金桥","云南昆明市"};
+			String[] school=new String[] {"第一大学","第二大学","第三大学","第四大学","第五大学","第六大学","第七大学","第八大学"};
+			String sql="INSERT INTO user(name,usex,address,date,person,girl,school,studentid,height,weight,gradeId) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+			pstmt=conn.prepareStatement(sql);
 			long begin = System.currentTimeMillis();
-			System.out.println("开始插入："+begin);
-			for(int i=0;i<20000000;i++) {
-				count=stmt.executeUpdate(sql);
+			System.out.println("开始插入："+begin);	
+			Random r=new Random();
+			SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			for(int i=0;i<1;i++) {//循环插入数据
+				int index=r.nextInt(name.length);
+				 int second=r.nextInt(5);
+				 int three=r.nextInt(school.length);
+				 int gradeId=r.nextInt(3)+1;
+//				 System.out.println(name[index]+"  =="+address[second]+"  =="+school[index]);
+				 pstmt.setString(1,name[index]);
+				 pstmt.setString(2,"宠物");
+				 pstmt.setString(3,address[second]);
+				 pstmt.setString(4,sdf.format(new java.util.Date()).toString());
+				 pstmt.setString(5,"生命体");
+				 pstmt.setString(6,"王柳");
+				 pstmt.setString(7,school[three]);
+				 pstmt.setString(8, "43052319981202");
+				 pstmt.setDouble(9, 11.8);
+				 pstmt.setDouble(10, 3.1);
+				 pstmt.setInt(11, gradeId);
+				count=pstmt.executeUpdate();
 			}
 			//锟结交锟斤拷锟斤拷
 			conn.commit();
 			long end=System.currentTimeMillis();
-			System.out.println("插入完成"+end);
+//			System.out.println("插入完成"+end);
 			System.out.println("所用时间："+(end-begin)/1000+"秒");
 		} catch (Exception e) {
 			//锟截癸拷锟斤拷锟斤拷
@@ -56,9 +78,9 @@ public class employee {
 			}
 			e.printStackTrace();
 		}finally {
-			if(stmt!=null) {
+			if(pstmt!=null) {
 				try {
-					stmt.close();
+					pstmt.close();
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -75,9 +97,9 @@ public class employee {
 		
 		}
 		if(count==1) {
-			System.out.println("锟斤拷喜锟斤拷注锟斤拷晒锟斤拷锟�success");
+			System.out.println("success");
 		}else {
-			System.out.println("锟斤拷锟脚猴拷锟斤拷注锟斤拷失锟杰ｏ拷fail");
+			System.out.println("fail");
 		}
 	}
 
